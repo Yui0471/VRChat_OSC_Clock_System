864851f86c54d8e5f079bd142581c411715d81cb
Yui0471 <wind.garden0471@gmail.com>
2022-06-24 19:16:52 +0900

    Simple-Betav2.1.1,暫定的に引数でのIP,ポート指定に対応

--- Analog_Clock_System_Stable_V2/script/ACS_OSC_Clock-Simple.py
+++ Analog_Clock_System_Stable_V2/script/ACS_OSC_Clock-Simple.py
@@ -3,23 +3,66 @@ import time
 import sys
 import datetime
 import math
+import ipaddress
 
 msg = """
 //////////////////////////////////////////
 VRChat Open Sound Control 
                   時刻表示プログラム
 
-Analog Clock System Stable Version 2.1
+Analog Clock System Beta Version 2.1.1
 
 //////////////////////////////////////////
 """
 
 #作成 : 風庭ゆい
-#最終更新 : 2022/04/17
+#最終更新 : 2022/06/24
 
 #ipとポートをセット
-ip = "127.0.0.1"
-port = 9000
+def is_integer(n):
+    return n.isascii() and n.isdecimal()
+
+def ip_check(values):
+    try:
+        ip_set = ipaddress.ip_address(values)
+
+        if type(ip_set) is ipaddress.IPv4Address:
+            ip = str(ip_set)
+            return ip
+
+        else:
+            return None
+    
+    except ValueError:
+        return None
+
+def port_check(values):
+    valid_flag = False
+
+    if is_integer(values):
+        port=int(values)
+
+        if 1 <= port <= 65535:
+            valid_flag = True
+
+    if not valid_flag:
+        return None
+
+    return port
+
+#ipとポートをセット
+if not (len(sys.argv) <= 2):
+    ip = ip_check(sys.argv[1])
+    port = port_check(sys.argv[2])
+
+    if not all([ip, port]):
+        print("ERROR!【引数が間違っているためデフォルトの数値を使用します】")
+        ip = "127.0.0.1"
+        port = 9000
+
+else:
+    ip = "127.0.0.1"
+    port = 9000
 
 client = SimpleUDPClient(ip, port)
 
--- Digital_Display_System_Stable_V2/script/DDS_OSC_Clock-Simple.py
+++ Digital_Display_System_Stable_V2/script/DDS_OSC_Clock-Simple.py
@@ -2,23 +2,66 @@ from pythonosc.udp_client import SimpleUDPClient
 import time
 import sys
 import datetime
+import ipaddress
 
 msg = """
 //////////////////////////////////////////
 VRChat Open Sound Control 
                   時刻表示プログラム
 
-Digital Display System Stable Version 2.1
+Digital Display System Beta Version 2.1.1
 
 //////////////////////////////////////////
 """
 
 #作成 : 風庭ゆい
-#最終更新 : 2022/04/17
+#最終更新 : 2022/06/24
+
+def is_integer(n):
+    return n.isascii() and n.isdecimal()
+
+def ip_check(values):
+    try:
+        ip_set = ipaddress.ip_address(values)
+
+        if type(ip_set) is ipaddress.IPv4Address:
+            ip = str(ip_set)
+            return ip
+
+        else:
+            return None
+    
+    except ValueError:
+        return None
+
+def port_check(values):
+    valid_flag = False
+
+    if is_integer(values):
+        port=int(values)
+
+        if 1 <= port <= 65535:
+            valid_flag = True
+
+    if not valid_flag:
+        return None
+
+    return port
 
 #ipとポートをセット
-ip = "127.0.0.1"
-port = 9000
+if not (len(sys.argv) <= 2):
+    ip = ip_check(sys.argv[1])
+    port = port_check(sys.argv[2])
+
+    if not all([ip, port]):
+        print("ERROR!【引数が間違っているためデフォルトの数値を使用します】")
+        ip = "127.0.0.1"
+        port = 9000
+
+else:
+    ip = "127.0.0.1"
+    port = 9000
+
 
 client = SimpleUDPClient(ip, port)
 
